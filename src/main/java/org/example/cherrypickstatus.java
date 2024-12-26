c9c881709228b6c735b4dd9dfa29c461b32f2432
Ajay Kumar <ajayk131989@gmail.com>
2024-12-26 15:39:12 +0530

    change

---
+++ src/main/java/org/example/cherrypickstatus.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class cherrypickstatus {
+
+    public void checkStatus(){
+        System.out.println("Wanted to cherry pick");
+    }
+
+    public void checkStatus2(){
+        System.out.println("Wanted to cherry pick");
+    }
+
+}
